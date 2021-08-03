2bf92f6f91bf0e9f32a098a56d669fa14b66a1f7
esenakyol <esen.akyol.88@gmail.com>
2021-08-02 21:37:38 -0500

    commit

---
+++ src/bugfighters/Practice01.java
@@ -0,0 +1,15 @@
+package bugfighters;
+
+public class Practice01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Bug Fighters");
+        //Git is a version control tool
+        //You can make changes and save them as version
+        //It is used to collaborate with others
+        // You can use all your versions and compare them
+        System.out.println("GitHub");
+
+
+
+    }
+}
