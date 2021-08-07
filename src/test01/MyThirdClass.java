fac054a94adc8be2d6aac14fb38bb6cc9add4a22
esenakyol <esen.akyol.88@gmail.com>
2021-08-07 13:45:21 -0500

    Committing mythirdclass

---
+++ src/test01/MyThirdClass.java
@@ -0,0 +1,11 @@
+package test01;
+
+public class MyThirdClass {
+    public static void main(String[] args) {
+        int x = 5;
+        int sum = x+2;
+        System.out.println(sum);
+
+        int y = 0;
+    }
+}
