73a99a4ebe53fa4b8b79c7e1156776224a879546
esenakyol <esen.akyol.88@gmail.com>
2021-08-08 13:39:26 -0500

    my local commit 1

---
+++ src/test01/MyLocalClass01.java
@@ -0,0 +1,8 @@
+package test01;
+
+public class MyLocalClass01 {
+    public static void main(String[] args) {
+        //I will create a local branch
+        //I will push the codes to the local branch
+    }
+}
